1addb3ca8039ab44e3825d644d2c34f80ea2c751
Ciaran Curley <ciaran.curley.11.11@gmail.com>
2012-10-18 16:27:22 +0100

    Modified TSPFile.cpp - Syntax fixes

--- TSPFile.cpp
+++ TSPFile.cpp
@@ -1,46 +1,47 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
+#include <cmath>
 #include <vector>
 using namespace std;
 
 typedef vector<float*> nodeList;
 
 //Parses and stores data from a TSBLIB95 .tsp file (EUC_2D Node types)
-class TSP{
+class TSPFile{
 	private:
 		nodeList list;
 	public:
-		void setNodeListFromFile;
-		float GetDistance;
-}
+		void SetNodeListFromTSPFile(string);
+		float GetDistance(float*, float*);
+};
 
-float GetDistance()
-{
-	xd = x[i] - x[j];
-	yd = y[i] - y[j];
-	return (nint(sqrt(xd*xd + yd*yd ));
-}
-
-
-void SetNodeListFromTSPFile(string filename){
+void TSPFile::SetNodeListFromTSPFile(string filename){
 	ifstream TSPFile;
 	stringstream currentLine;
 	int linePos;
 	int dumpInt;
 	TSPFile.open(filename);
-	while(currentLine.str != "NODE_COORD_SECTION"){
-		getline(TSPFile, currentLine.str);
+	while(currentLine.str() != "NODE_COORD_SECTION"){
+		getline(TSPFile, currentLine.str());
 	}
-	list = new nodeList;
+	list
 	linePos = 1;
-	while(currentLine.str != "EOF"){
-		getline(TSPFile, currentLine);
+	while(currentLine.str() != "EOF"){
+		getline(TSPFile, currentLine.str());
 		currentLine >> dumpInt;
 		if(linePos == dumpInt){
-			list.push_back(new float[2]) 
+			list.push_back(new float[2]);
 			currentLine >> list.back[0];
 			currentLine >> list.back[1];
 		}
 	}
+}
+
+float TSPFile::GetDistance(float* x, float* y)
+{
+	float xd = x[i] - x[j];
+	float yd = y[i] - y[j];
+	return (nint(sqrt(xd*xd + yd*yd ));
 }
