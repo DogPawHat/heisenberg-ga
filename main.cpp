9c74304f2d5fd12ab0220b4dc22e0a5efb328149
Ciaran Curley <ciaran.curley.11.11@gmail.com>
2012-10-26 14:57:14 +0100

    Merged TSP.cpp into TSP.hpp (not sure if that is best practice). All errors in main.cpp are fixed - in theroy. TSP.hpp still reqires more work.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <TSP.hpp>
 
 int main(int argc, char** argv){
-	TSP *TSPGraph = new TSP(argv[0]);
-	for (double* : TSP::list){
+	TSP TSPGraph (argv[0]);
+	for (double* i: TSPGraph.list() ){
 		for (double j : i){
-			std::cout << i << ' ';
+			std::cout << j << ' ';
 		}
-		std::cout << '/n';
+		std::cout << '\n';
 	}
 }
