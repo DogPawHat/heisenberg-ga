1cb19289614bbc4549b48d2cbb5b50becb5f5fe0
Ciaran Curley <ciaran.curley.11.11@gmail.com>
2012-11-27 02:25:06 +0000

    Meh

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include "TSP.h"
+#include <vector>
 #include <iostream>
 #include <fstream>
 
 int main(int argc, char** argv){
 	TSP TSPGraph (argv[1]);
-	for (std::vector<double> i: TSPGraph.list()){
-		std::cout << i[0] << ' ' << i[1] << std::endl;
+	char l;
+	std::vector< std::vector<double> > list = TSPGraph.TSP::list();
+	for (unsigned int i = 0; i < list.size(); i++){
+		std::cout << list[i][0] << ' ' << list[i][1] << std::endl;
 	}
+	std::cout << "Press any key to exit";
+	std::cin >> l;
 }
