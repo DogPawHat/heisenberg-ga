1daf6b4872af2d5e3be4712016f005e8aec79a89
Ciaran Curley <ciaran.curley.11.11@gmail.com>
2012-12-04 23:22:36 +0000

    Added a convert from 2d Vector to 2d array class.
    The array will be easier to deal with in cuda memory

--- main.cpp
+++ main.cpp
@@ -5,14 +5,16 @@
 #include "TSPList.h"
 
 
-/*
+
 int main(int argc, char** argv){
 	TSP TSPGraph (argv[1]);
-	TSPList list = TSPGraph.TSP::list();
+	TSPList list = TSPGraph.list();
+	double** test2D = TurnTSPListTo2DArray(list);
+
 	for (unsigned int i = 0; i < list.size(); i++){
-		std::cout << list[i][0] << ' ' << list[i][1] << std::endl;
+		std::cout << test2D[i][0] << ' ' << test2D[i][1] << std::endl;
 	}
 	std::cout << "Press any key to exit";
 	std::cin >> new char;
+	delete test2D;
 }
-*/
