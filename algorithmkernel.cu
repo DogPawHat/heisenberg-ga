eea85827789677813f736df2deced89510d07787
Ciaran Curley <ciaran.curley.11.11@gmail.com>
2013-02-01 11:26:58 +0000

    derpahurp

--- algorithmkernel.cu
+++ algorithmkernel.cu
@@ -1,25 +1,41 @@
 #include <cuda.h>
 #include <math.h>
 
-__device__ void selection(int * matingPool, int * islandPoplulation, float * tspGraph, const int chromosoneSize, const int islandPopLength, const int graphSize){
+__device__ float * selection(int * matingPool, int * islandPoplulation, float * tspGraph, const int chromosoneSize, const int islandPopLength, const int graphSize){
 	__shared__ float fitnessValues[popLength];
 	__shared__ float totalFinessValue;
+	float selectedChromosome[chromosomeSize];
 	int start = islandPoplulation[threadIdx.x*chromosoneSize];
 	for(int i = 1; i < chromosoneSize; i++){
 		float xd = tspGraph[start+(i*2)] - tspGraph[start+(i*2)-2];
 		float yd = tspGraph[start+(i*2)+1] - tspGraph[start+(i*2)-1];
 		fitnessValues[threadIdx.x] += sqrt(xd^2 + yd^2);
 		totalFitnessValue += sqrt(xd^2 + yd^2);
+		__syncthreads();
 	}
 	fitnessValues[threadIdx.x] = finessValues[threadIdx.x]/totalFitnessValue;
-	
-	
-	
+	__syncthreads();
+
+	float rulettball = randomRoulletBall(); //spell rulett properly when your sober :D
+	unsigned float diff = fitnessValues[threadIdx.x] - rulettball;
+	memcpy(selectedIndividual, &islandPopulation[threadIdx.x*chromosomeSize], sizeof(int)*chromosomeSize);
+
+	for(int i = 0; i < popLength; i++){
+		if(diff < fitnessValues[i] - rulettball;){
+			diff = itnessValues[i] - rulettball;
+			memcpy(selectedChromosome, &islandPopulation[i*chromosomeSize], sizeof(int)*chromosomeSize);
+		}
+		__syncthreads();
+	}
+	return selectedIndividual;
 }
 
 __device__ void generation(islandPopulation, poplulation, tspGraph, chromosoneSize, popLength, graphSize, popMultipler){
-	__shared__ int matingPool[blockDim.x*chromosoneSize];
-	selection(matingPool, islandPopulation, tspGraph, chromosoneSize, popLength/gridDim.x, graphSize);
+	__shared__ float matingPool[blockDim.x*chromosoneSize];
+	memcpy(&matingPool[threadIdx.x*chromosoneSize], &selection(matingPool, islandPopulation, tspGraph, chromosoneSize, popLength/gridDim.x, graphSize), sizeof(float)*chromosomeSize);
+	for(int i=0; i < popMultiplier; i++){
+		memcpy(&islandPopulation[(threadIdx*popMultiplier+i)*chromosomeSize], &poplulation[(gridIndex*popMultiplier+i)*chromosomeSize], chromosomesize*sizeof(int));
+	}
 }
 
 
@@ -31,6 +47,6 @@ __global__ void runGeneticAlgorithm(int * poplulation, float * tspGraph, const i
 	}
 	generation(islandPopulation, poplulation, tspGraph, chromosoneSize, popLength, graphSize, popMultipler);
 	for(int j = 0; j < chromosoneSize; j++){
-		poplulation[threadIdx.x + blockDim.x*blockIdx.x + i] = matingPool[blockDim.x + i];
+		poplulation[threadIdx.x + blockDim.x*blockIdx.x + i] = islandPoplulation[blockDim.x + i];
 	}
 }
