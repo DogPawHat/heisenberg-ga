745f642385801b03d3b42a5e3d0c582ce1618ba7
DogPawHat <ciaran.curley.11.11@gmail.com>
2013-02-04 17:10:41 -0600

    durpadurp

--- algorithmkernel.cu
+++ algorithmkernel.cu
@@ -2,9 +2,16 @@
 #include <math.h>
 #include "ga_struts.h"
 
+__device__ float randomRouletteBall(){
+	thrust::minstd_rand rng;
+	thrust::uniform_real_distribution<float> dist(0, 1000);
+	return dist(rnt);
+}
+
 __device__ float * selection(int * matingPool, int * islandPoplulation, deviceFields fields, fieldSizes sizes){
 	__shared__ float fitnessValues[sizes.populationSize];
 	__shared__ float totalFinessValue;
+	thrust:minstd_rand rng;
 	float selectedChromosome[sizes.chromosomeSize];
 	int start = islandPoplulation[threadIdx.x*sizes.chromosoneSize];
 	for(int i = 1; i < sizes.chromosoneSize; i++){
@@ -17,14 +24,14 @@ __device__ float * selection(int * matingPool, int * islandPoplulation, deviceFi
 	fitnessValues[threadIdx.x] = finessValues[threadIdx.x]/totalFitnessValue;
 	__syncthreads();
 
-	float rouletteBall = randomrouletteBall();
+	float rouletteBall = randomRouletteBall();
 	unsigned float diff = fdif(fitnessValues[threadIdx.x], rouletteBall);
 	memcpy(selectedIndividual, &islandPopulation[threadIdx.x*chromosomeSize], sizeof(int)*chromosomeSize);
 
 	for(int i = 0; i < sizes.poplulationSize; i++){
 		if(diff < fitnessValues[i] - rouletteBall){
 			fdif(fitnessValues[threadIdx.x], rouletteBall);
-			memcpy(selectedChromosome, &islandPopulation[i*chromosomeSize], sizeof(int)*chromosomeSize);
+			memcpy(selectedChromosome, &islandPopulation[i*chromosomeSize], sizeof(int)*sizes.chromosomeSize);
 		}
 		__syncthreads();
 	}
@@ -33,7 +40,7 @@ __device__ float * selection(int * matingPool, int * islandPoplulation, deviceFi
 
 __device__ void generation(int * islandPopulation, deviceFields fields, fieldSizes sizes){
 	__shared__ float matingPool[sizes.populationSize/10];
-	memcpy(&matingPool[threadIdx.x*chromosoneSize], selection(matingPool, islandPopulation, fields, sizes), sizeof(float)*chromosomeSize);
+	memcpy(&matingPool[threadIdx.x*sizes.chromosomeSize], selection(matingPool, islandPopulation, fields, sizes), sizeof(float)*chromosomeSize);
 	__syncthreads();
 	for(int i=0; i < popMultiplier; i++){
 		memcpy(&islandPopulation[(threadIdx*popMultiplier+i)*chromosomeSize], &matingPool[threadIdx.x*chromosoneSize], chromosomesize*sizeof(int));
@@ -43,8 +50,8 @@ __device__ void generation(int * islandPopulation, deviceFields fields, fieldSiz
 
 __global__ void runGeneticAlgorithm(deviceFields fields, fieldSizes sizes){
 	int gridIndex = threadIdx.x + blockDim.x*blockIdx.x;
-	__shared__ int * islandPopulation = (int*) malloc(sizes.populationSizes)*sizes.chromosoneSize)];
-	memcpy(&islandPopulation[threadIdx*chromosomeSize], &poplulation[gridIndex*chromosomeSize], chromosoneSize*sizeof(int));
+	__shared__ int islandPopulation[sizes.populationSize];
+	memcpy(&islandPopulation[threadIdx*chromosomeSize], &fields.poplulation[gridIndex*chromosomeSize], sizes.chromosoneSize*sizeof(int));
 	generation(islandPopulation, fields, sizes);
-	memcpy(&poplulation[gridIndex*chromosomeSize], &islandPoplulation[blockDim.x*chromosomeSize], chromosomeSize*sizeof(int));
+	memcpy(&fields.lpoplulation[gridIndex*chromosomeSize], &islandPoplulation[blockDim.x*chromosomeSize], sizes.chromosomeSize*sizeof(int));
 }
