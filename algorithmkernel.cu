6d2881a7b570459218374db14038332dadd42377
Ciaran Curley <ciaran.curley.11.11@gmail.com>
2013-03-06 09:02:11 +0000

    Made some changes.

--- algorithmkernel.cu
+++ algorithmkernel.cu
@@ -4,15 +4,51 @@
 #include <thrust/random/uniform_real_distribution.h>
 #include "global_structs.h"
 
-/*
-__device__ __forceinline__ float randomRouletteBall(deviceFields fields){
+
+__device__  void crossover(short*, short*, deviceFields);
+__device__  void selection(short*, short*, deviceFields);
+
+__device__ __forceinline__ void generation(short * islandPopulation, deviceFields fields){
+	__shared__ short selectedMates[TOTAL_ISLAND_POPULATION_MEMORY_SIZE];
+	selection(selectedMates, islandPopulation, fields);
+	__syncthreads();
+
+	for(short i = 0; i < CHROMOSOME_SIZE; i++){
+		islandPopulation[threadIdx.x*CHROMOSOME_SIZE+i] = selectedMates[threadIdx.x*CHROMOSOME_SIZE+i];
+	}
+}
+
+
+__global__ void runGeneticAlgorithm(deviceFields fields){
+	int gridIndex = threadIdx.x + blockDim.x*blockIdx.x;
+	__shared__ short islandPopulation[TOTAL_ISLAND_POPULATION_MEMORY_SIZE];
+
+	for(short i = 0; i < CHROMOSOME_SIZE; i++){
+		islandPopulation[threadIdx.x*CHROMOSOME_SIZE+i] = fields.population[gridIndex*CHROMOSOME_SIZE+i];
+	}
+	__syncthreads();
+
+	generation(islandPopulation, fields);
+	__syncthreads();
+
+	for(short i = 0; i < CHROMOSOME_SIZE; i++){
+		fields.population[gridIndex*CHROMOSOME_SIZE+i] = islandPopulation[threadIdx.x*CHROMOSOME_SIZE+i];
+	}
+	__syncthreads();
+}
+
+
+
+/*Selection Functions*/
+
+__device__  float randomRouletteBall(deviceFields fields){
 	thrust::minstd_rand0 rng(fields.seeds[threadIdx.x + blockDim.x*blockIdx.x]);
 	thrust::uniform_real_distribution<float> dist(0, 1);
 	float result = dist(rng);
 	return result;
 }
 
-__device__ __forceinline__ void selection(short* selectedMates, short* islandPopulation, deviceFields fields){
+__device__  void selection(short* selectedMates, short* islandPopulation, deviceFields fields){
 	__shared__ float fitnessValues[ISLAND_POPULATION_SIZE];
 	__shared__ float sumOfFitnessValues[ISLAND_POPULATION_SIZE];
 	short start = threadIdx.x*CHROMOSOME_SIZE;
@@ -62,37 +98,54 @@ __device__ __forceinline__ void selection(short* selectedMates, short* islandPop
 		}
 	}
 }
-*/
 
 
-__device__ __forceinline__ void crossover(short* selectedMates, short* islandPopulation, deviceFields fields);
-__device__ __forceinline__ void selection(short* selectedMates, short* islandPopulation, deviceFields fields);
+/*Genetic Operators*/
 
-__device__ __forceinline__ void generation(short * islandPopulation, deviceFields fields){
-	__shared__ short selectedMates[TOTAL_ISLAND_POPULATION_MEMORY_SIZE];
-	selection(selectedMates, islandPopulation, fields);
-	__syncthreads();
+__device__ __forceinline__ void crossover(short* selectedMates, short* islandPopulation, deviceFields fields){
+	/*We need two different paths here beause each thread needs two parents to generate a single offspring.
+	The first half of the block will take one parent from the first half of islandPopulation, while the second parent
+	will come from the second half. This is reversed for the second half of the block. To reduce warp control divergence,
+	block size shoud be a multiple of 2*warp size, 32 being the current value of warps in Fermi and Kepler GPU's*/
+	
+	short* parent1; //Points to the first element in the chromosome of parent1
+	short* parent2;
+	short point1;
+	short point2;
+	short size;
+	short offspring[52];
+	thrust::minstd_rand0 rng(fields.seeds[threadIdx.x+blockDim.x*blockIdx.x]);
+	thrust::uniform_int_distribution<short> dist1;
+	thrust::uniform_int_distribution<short> dist2;
 
-	for(short i = 0; i < CHROMOSOME_SIZE; i++){
-		islandPopulation[threadIdx.x*CHROMOSOME_SIZE+i] = selectedMates[threadIdx.x*CHROMOSOME_SIZE+i];
+	if(threadIdx.x < (BLOCK_SIZE/2)){
+		parent1 = selectedMates[threadIdx.x*CHROMOSOME_SIZE];
+		parent2 = selectedMates[(threadIdx.x+(BLOCK_SIZE/2))*CHROMOSOME_SIZE];
+	}else{
+		parent1 = selectedMates[threadIdx.x*CHROMOSOME_SIZE];
+		parent2 = selectedMates[(threadIdx.x-(BLOCK_SIZE/2))*CHROMOSOME_SIZE];
 	}
-}
 
+	dist1 = thrust::uniform_int_distribution<short>(0, 52);
+	short point1 = dist1(rng);
+	dist2 = thrust::uniform_int_distribution<short>(point1, 52)
+	short point2 = dist2(rng);
 
-__global__ void runGeneticAlgorithm(deviceFields fields){
-	int gridIndex = threadIdx.x + blockDim.x*blockIdx.x;
-	__shared__ short islandPopulation[TOTAL_ISLAND_POPULATION_MEMORY_SIZE];
-
-	for(short i = 0; i < CHROMOSOME_SIZE; i++){
-		islandPopulation[threadIdx.x*CHROMOSOME_SIZE+i] = fields.population[gridIndex*CHROMOSOME_SIZE+i];
+	for(short i = point1; i <= point2; i++){
+		offspring[i] = parent2[i];
 	}
-	__syncthreads();
 
-	generation(islandPopulation, fields);
-	__syncthreads();
-
-	for(short i = 0; i < CHROMOSOME_SIZE; i++){
-		fields.population[gridIndex*CHROMOSOME_SIZE+i] = islandPopulation[threadIdx.x*CHROMOSOME_SIZE+i];
+	for(int i = 0; i < point1; i++){
+		for(int j = 0; j < (point2 - point1); j++){
+			if(parent1[i] == offspring[j]){
+				offspring[i] == parent2[i];
+				goto a;
+			}
+		}
+		offspring[i] == parent1[i];
 	}
-	__syncthreads();
+
+	for(int i = 0; i < CHROMOSOME_SIZE; i++){
+		islandPopulation[threadIdx*CHROMOSOME_SIZE+i] = offspring[i];
+	}
 }
